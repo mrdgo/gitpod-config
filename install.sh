2718c719e200f32e19cb8ce7b30aa36d9b155140
Maxim R. v. Onciul <maxim.ritter.von.onciul@gmail.com>
2022-11-27 16:24:29 +0100

    add my dots and haskell language service

--- install.sh
+++ install.sh
@@ -1,45 +1,33 @@
 #!/usr/bin/env bash
 
+# install rsync to deploy dotfiles
+sudo apt install --yes rsync libicu-dev libncurses-dev libgmp-dev zlib1g-dev
+
+wget https://downloads.haskell.org/~hls/haskell-language-server-1.8.0.0/haskell-language-server-1.8.0.0-x86_64-linux-deb10.tar.xz
+tar -xvf haskell-language-server-1.8.0.0-x86_64-linux-deb10.tar.xz
+export PATH="$PATH:$(pwd)/haskell-language-server-1.8.0.0/bin"
+
+
 cd $(mktemp -d)
 
-URL="https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
-if test -n "$NEOVIM_VERSION"
-then
-    URL="https://github.com/neovim/neovim/releases/download/$NEOVIM_VERSION/nvim.appimage"
-fi
-
+URL="https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage"
 curl -LO "$URL"
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract >/dev/null
 mkdir -p /home/gitpod/.local/bin
 ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
 
-# Benchmarking different ways of installing nvim
-
-# NIXSTART=$(date +%s.%N)
-# source $HOME/.profile
-# nix-env -iA nixpkgs.neovim
-# NIXEND=$(date +%s.%N)
-# NIXDIFF=$(echo "$NIXEND - $NIXSTART" | bc)
-# echo "Install with nix took $NIXDIFF" 1>&2
-# # Install with nix took 12.794308385
-
-
-# APTSTART=$(date +%s.%N)
-# sudo apt install --yes neovim
-# APTEND=$(date +%s.%N)
-# APTDIFF=$(echo "$APTEND - $APTSTART" | bc)
-# echo "Install with apt took $APTDIFF" 1>&2
-# # Install with apt took 5.360583211
-
-
-# APPIMAGESTART=$(date +%s.%N)
-# curl -LO "https://github.com/neovim/neovim/releases/${${NVIM_VERSION:+download/$NVIM_VERSION}:-latest/download}/nvim.appimage"
-# chmod u+x nvim.appimage
-# ./nvim.appimage --appimage-extract >/dev/null
-# mkdir -p /home/gitpod/.local/bin
-# ln -s $(pwd)/squashfs-root/AppRun /home/gitpod/.local/bin/nvim
-# APPIMAGEEND=$(date +%s.%N)
-# APPIMAGEDIFF=$(echo "$APPIMAGEEND - $APPIMAGESTART" | bc)
-# echo "Install as appimage took $APPIMAGEDIFF" 1>&2
-# # Install as appimage took 3.029663746
+pushd /opt
+sudo git clone \
+  --depth 1  \
+  --filter=blob:none  \
+  --sparse \
+  https://github.com/mrdgo/dots \
+  /opt/dots \
+;
+sudo chown -R gitpod:gitpod dots
+cd dots
+git sparse-checkout set roles/nvim/files/nvim
+mkdir -p ~/.config/nvim
+rsync -a roles/nvim/files/nvim/ ~/.config/nvim
+popd
